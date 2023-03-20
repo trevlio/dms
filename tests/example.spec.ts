daf18902637ca9bc51adbb0c40bbeccd4b3d1fa1
Blake Seipler <bseipler@yahoo.com>
2023-03-19 23:56:54 -0400

    chore: remove tests

--- tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has title', async ({ page }) => {
-  await page.goto('localhost:3000');
-  const locator = page.locator('h1');
-  await expect(locator).toContainText('Hello World');
-});
